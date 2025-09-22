5c98eb56e579b7506a8f5635294aec7cdac0ae21
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-09-22 17:20:23 -0400

    refac: separate heap and main
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CFLAGS=-Wall -Wextra -std=c11 -pedantic -g
 
-heap: main.c
-	cc $(CFLAGS) -o heap main.c
+heap: main.c heap.c heap.h
+	cc $(CFLAGS) -o heap main.c heap.c
---
+++ heap.h
@@ -0,0 +1,40 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define CHUNK_LIST_CAP 1024
+#define HEAP_CAP 640000
+
+static_assert(HEAP_CAP % sizeof(uintptr_t) == 0, 
+    "The heap capacity is not divisible by the size of a word of the OS (platform)");
+extern uintptr_t heap[HEAP_CAP];
+
+typedef struct Chunk {
+    uintptr_t *start;
+    size_t size;
+} Chunk;
+
+typedef struct {
+    size_t count;
+    Chunk chunks[CHUNK_LIST_CAP];
+} Chunk_List;
+
+extern Chunk_List alloced_chunks;
+extern Chunk_List freed_chunks;
+extern Chunk_List tmp_chunks;
+
+void *heap_alloc(size_t size_bytes);
+void heap_free(void *ptr);
+void heap_collect(void);
+
+void chunk_list_insert(Chunk_List *list, void *start, size_t size);
+void chunk_list_merge(Chunk_List *dst, const Chunk_List *src);
+void chunk_list_dump(const Chunk_List *list);
+int chunk_list_find(const Chunk_List *list, uintptr_t *ptr);
+void chunk_list_remove(Chunk_List *list, size_t index);
+
+#endif // HEAP_H
